47ffd7b2f0db0c625fdb6755a4d8c3bdc9ba8e8f
muehung <hung.co24@gmail.com>
2023-03-25 17:53:40 +0800

    環境整理：安裝bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+
+
+import './assets/css/main.css'
 
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
 
