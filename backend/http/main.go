e3882accbd7ec30272e0a65079543129db004067
Artyom Liou <artyomliou@gmail.com>
2023-04-02 11:55:02 +0800

    feat: debug page

--- backend/http/main.go
+++ backend/http/main.go
@@ -1,11 +1,17 @@
 package http
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() (router *gin.Engine) {
 	router = gin.Default()
+
+	if os.Getenv("APP_DEBUG") == "1" {
+		router.StaticFile("/debug", "./public/debug.html")
+	}
 
 	api := router.Group("/api")
 	{
