42a7ed72dbaffc75fbf244b2275ebe9d8e768bb2
muehung <hung.co24@gmail.com>
2023-04-02 23:55:45 +0800

    fix (footer, navbar): replace components on  global

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,19 @@
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import FooterComponent from './components/Footer.vue'
+import Navbar from './components/Navbar.vue'
+
 import 'bootstrap'
-
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/css/main.css'
 
 const app = createApp(App)
-app.use(createPinia())
-app.use(router)
+app.use(createPinia()).use(router)
+
+app.component('Navbar', Navbar).component('FooterComponent', FooterComponent)
 
 app.mount('#app')
