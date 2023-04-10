d3003225bbfbaba37fec41c944747434bce34ab7
muehung <hung.co24@gmail.com>
2023-04-10 12:06:52 +0800

    feat: check login action store for every pages

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -17,3 +17,20 @@ app.use(createPinia()).use(router)
 app.component('Navbar', Navbar).component('FooterComponent', FooterComponent)
 
 app.mount('#app')
+
+// every pages
+import { mapActions } from 'pinia'
+import { useLoginCheckStore } from './stores/useLoginCheck.js'
+
+export default {
+    data() {
+        return {}
+    },
+    computed: {},
+    methods: {
+        ...mapActions(useLoginCheckStore, ['fetchLoginApi'])
+    },
+    created() {
+        this.fetchLoginApi()
+    }
+}
