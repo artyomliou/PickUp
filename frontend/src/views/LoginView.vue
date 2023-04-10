48d904f6f336ab54eb168dec47f0e1c2d7fba588
muehung <hung.co24@gmail.com>
2023-04-10 12:21:50 +0800

    feat: login page form sumit, bootstrap modal testing

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -31,8 +31,12 @@
                                 />
                             </label>
                         </div>
-                        <button type="submit" class="btn btn-md mx-auto btn-sumit">
-                            送出 {{ isLogined }}
+                        <button
+                            @click="loginSubmit()"
+                            type="submit"
+                            class="btn btn-md mx-auto btn-sumit"
+                        >
+                            送出
                         </button>
                     </form>
                     <div class="text-center">
@@ -44,8 +48,61 @@
         </div>
     </section>
     <FooterComponent />
+
+    <!--  -->
+    <div ref="modalAlert" class="modal" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">modal Alert</h5>
+                    <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                    ></button>
+                </div>
+                <div class="modal-body">
+                    <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                        Close
+                    </button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div ref="modalAlert2" class="modal" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">modalAlert2</h5>
+                    <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                    ></button>
+                </div>
+                <div class="modal-body">
+                    <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                        Close
+                    </button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Modal from 'bootstrap/js/dist/modal'
 import { mapActions, mapState } from 'pinia'
 import { useLoginCheckStore } from '../stores/useLoginCheck.js'
 
@@ -53,10 +110,8 @@ export default {
     data() {
         return {
             userLogin: {
-                userId: 0,
                 email: '',
                 password: ''
-                // isStatus: isStatus
             }
         }
     },
@@ -64,10 +119,45 @@ export default {
         ...mapState(useLoginCheckStore, ['isLogined'])
     },
     methods: {
-        ...mapActions(useLoginCheckStore, ['fetchLoginApi'])
+        ...mapActions(useLoginCheckStore, ['fetchLoginApi']),
+        async loginSubmit() {
+            try {
+                const response = await fetch('http://localhost:5000/api/login', {
+                    method: 'POST',
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        email: this.userLogin.email,
+                        password: this.userLogin.password
+                    })
+                })
+                // 拿到狀態 console.log(response, response.status)
+
+                const modalAlert = new Modal(this.$refs.modalAlert)
+                if (response.ok) {
+                    this.fetchLoginApi()
+                    console.log('ok:' + response.status)
+                    console.log("ok's action: " + this.isLogined)
+                    console.log(this.$refs.modalAlert)
+                    modalAlert.show()
+                    // this.$refs.modalWarning.show()
+                    this.$router.push('/')
+                } else {
+                    const resData = await response.json()
+                    console.log(resData, response.status)
+                    // this.$refs.modalAlert2.show()
+                    modalAlert.show()
+                    console.log(this.$refs.modalAlert2)
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
     },
     created() {
-        this.fetchLoginApi()
+        // this.fetchLoginApi()
     }
 }
 </script>
