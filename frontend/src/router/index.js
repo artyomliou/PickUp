1b3060f7a8aa3b04f25fcc5ec264a5512fbefb9d
muehung <hung.co24@gmail.com>
2023-04-10 12:09:03 +0800

    feat: index page with temporary content

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,21 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import PasswordView from '../views/PasswordView.vue'
+import index from '../views/index.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView
-    },
-    {
-      path: '/password',
-      name: 'password',
-      component: PasswordView
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'index',
+            component: index
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView
+        },
+        {
+            path: '/password',
+            name: 'password',
+            component: PasswordView
+        }
+    ]
 })
 
 export default router
