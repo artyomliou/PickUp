42af86cf3a61b20aed574da2513a5c1685e2543f
Artyom Liou <artyomliou@gmail.com>
2023-04-20 23:25:05 +0800

    feat: check login status for login-page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { isLoggedIn } from '../api'
 import LoginView from '../views/LoginView.vue'
 import PasswordView from '../views/PasswordView.vue'
 import index from '../views/index.vue'
+
+/**
+ * 只允許「沒登入的 user」進入這頁面
+ */
+async function isGuest() {
+    if (await isLoggedIn()) {
+        return {
+            path: '/'
+        }
+    }
+    return true
+}
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,7 +27,8 @@ const router = createRouter({
         {
             path: '/login',
             name: 'login',
-            component: LoginView
+            component: LoginView,
+            beforeEnter: [isGuest]
         },
         {
             path: '/password',
