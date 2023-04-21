3424fbbd52782bef16154e0cfb5ab229a262de9e
muehung <hung.co24@gmail.com>
2023-04-21 15:25:01 +0800

    feat: routes, new notFound page, beforeEach test

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import PasswordView from '../views/PasswordView.vue'
 import index from '../views/index.vue'
+import notFound from '../views/notFound.vue'
+
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +11,7 @@ const router = createRouter({
         {
             path: '/',
             name: 'index',
+            alias: ['/home, /index'],
             component: index
         },
         {
@@ -20,8 +23,34 @@ const router = createRouter({
             path: '/password',
             name: 'password',
             component: PasswordView
-        }
+        },
+        { 
+            // form Kuro https://book.vue.tw/CH4/4-2-route-settings.html
+            path: '/:pathMatch(.*)*',
+            name: 'notFound',
+            component: notFound
+        },
     ]
-})
+});
+
+// 每一個 router 都會經過
+// router.beforeEach((to) => {
+//     const store = useLoginCheckStore()
+//     if(to.meta.requiresAuth && !store.isLogined)
+//     return '/login'
+// })
+
+// router.beforeEach( async(to) => {
+//     //如果已登入(isLogined)就轉到首頁
+//     const store = useLoginCheckStore()
+//     if(
+//         store.isLogined && //檢查登入
+//         to.name !== 'login'
+//     ) {
+//         return { name: 'index'}
+//     }
+// });
+// // note: meta https://router.vuejs.org/zh/guide/advanced/meta.html
+
 
 export default router
---
+++ frontend/src/views/notFound.vue
@@ -0,0 +1,13 @@
+<template>
+    <NavbarComponent />
+    <section class="sec-h">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 text-center pt-5">
+                    <h1>Not Found</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <FooterComponent />
+</template>
