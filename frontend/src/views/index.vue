0fa36742965c3fb342e1bff3e224318bd64b346b
muehung <hung.co24@gmail.com>
2023-04-27 23:05:34 +0800

    feat: component footer, navbar

--- frontend/src/views/index.vue
+++ frontend/src/views/index.vue
@@ -1,16 +1,10 @@
-<script setup>
-// import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-    <NavbarComponent />
     <section class="sec-h text-center">
         <h1>這裡是首頁！</h1>
         <h1>這裡是首頁！</h1>
         <h1>這裡是首頁！</h1>
         <h1>這裡是首頁！</h1>
     </section>
-    <FooterComponent />
 </template>
 
 <style scoped></style>
