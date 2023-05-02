c154da98eccb454d8c4b992c67938cbad29a2bf8
Artyom Liou <artyomliou@gmail.com>
2023-05-02 22:26:58 +0800

    feat: order ctl

---
+++ backend/internal/httpapi/order_controller.go
@@ -0,0 +1,41 @@
+package httpapi
+
+import (
+	"the-video-project/backend/internal/db"
+	"the-video-project/backend/internal/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+type OrderController struct{}
+
+func (ctl OrderController) CreateOrder(c *gin.Context) {
+	// TODO
+}
+
+func (ctl OrderController) GetOrderStatus(c *gin.Context) {
+	order := &models.Order{}
+	tx := db.Conn().Find(&order, c.Param("orderId"))
+	if tx.Error != nil {
+		c.JSON(404, gin.H{
+			"message": "指定店家不存在",
+		})
+	} else {
+		c.JSON(200, gin.H{
+			"status": order.Status,
+		})
+	}
+}
+func (ctl OrderController) GetOrder(c *gin.Context) {
+	order := &models.Order{}
+	tx := db.Conn().Find(&order, c.Param("orderId"))
+	if tx.Error != nil {
+		c.JSON(404, gin.H{
+			"message": "指定店家不存在",
+		})
+	} else {
+		c.JSON(200, gin.H{
+			"order": order,
+		})
+	}
+}
