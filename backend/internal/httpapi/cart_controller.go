52d18d4e1e12eb01cee89658e3f6ecc8f420cd51
Artyom Liou <artyomliou@gmail.com>
2023-05-04 23:59:28 +0800

    feat: update api spec & impl

--- backend/internal/httpapi/cart_controller.go
+++ backend/internal/httpapi/cart_controller.go
@@ -1,51 +1,139 @@
 package httpapi
 
 import (
-	"strconv"
+	"log"
 	"the-video-project/backend/internal/db"
+	"the-video-project/backend/internal/httpapi/resp"
 	"the-video-project/backend/internal/models"
+	"the-video-project/backend/internal/snowflake"
 
 	"github.com/gin-gonic/gin"
 )
 
-type CartController struct{}
+type (
+	CartController struct{}
+	CartInput      struct {
+		StoreId uint `form:"storeId" binding:"required" valid:"int"`
+	}
+	ItemInput struct {
+		StoreId uint `form:"storeId" binding:"required" valid:"int"`
+		ItemId  uint `form:"itemId" binding:"required" valid:"int"`
+	}
+)
 
 func (ctl CartController) ListItem(c *gin.Context) {
-	userId := c.GetUint("uid")
-	storeId, err := strconv.Atoi(c.Param("storeId"))
-	if err != nil {
-		c.JSON(400, gin.H{
-			"message": "Store ID 必須是數字",
-		})
-		return
+	// input validation
+	input := CartInput{}
+	if err := c.BindUri(&input); err != nil {
+		c.AbortWithStatusJSON(400, resp.StdErrorResp)
 	}
 
-	cart := models.Cart{}
-	tx := db.Conn().Where(models.Cart{
-		UserId:  userId,
-		StoreId: uint(storeId),
-	}).Attrs(models.Cart{
-		Items: "",
-	}).FirstOrCreate(&cart)
-	if tx.Error != nil {
-		c.JSON(500, gin.H{
-			"message": "寫入失敗",
-		})
-	} else {
-		c.JSON(200, gin.H{
-			"items": cart.Items,
-		})
+	// db operation
+	cart, err := models.GetCart(c.GetUint("uid"), input.StoreId)
+	if err != nil {
+		log.Println(err)
+		c.AbortWithStatusJSON(500, resp.StdErrorResp)
 	}
+
+	c.AbortWithStatusJSON(200, gin.H{
+		"items": cart.Items,
+	})
 }
 func (ctl CartController) NewItem(c *gin.Context) {
-	// TODO
+	// input validation
+	input := CartInput{}
+	if err := c.BindUri(&input); err != nil {
+		log.Println(err)
+		c.AbortWithStatusJSON(400, resp.StdErrorResp)
+	}
+
+	item := models.CartItem{}
+	if err := c.Bind(&item); err != nil {
+		log.Println(err)
+		c.AbortWithStatusJSON(400, resp.StdErrorResp)
+	}
+
+	// db operation
+	cart, err := models.GetCart(c.GetUint("uid"), input.StoreId)
+	if err != nil {
+		log.Println(err)
+		c.AbortWithStatusJSON(500, resp.StdErrorResp)
+	}
+
+	item.ID = snowflake.Generate()
+	item.CartId = cart.ID
+	if tx := db.Conn().Save(&item); tx.Error != nil {
+		log.Println(tx.Error)
+		c.AbortWithStatusJSON(500, resp.StdErrorResp)
+	}
+
+	c.AbortWithStatusJSON(200, gin.H{
+		"cartItemid": item.ID,
+	})
 }
 func (ctl CartController) GetItem(c *gin.Context) {
-	// TODO
+	// input validation
+	input := ItemInput{}
+	if err := c.BindUri(&input); err != nil {
+		log.Println(err)
+		c.AbortWithStatusJSON(400, resp.StdErrorResp)
+	}
+
+	// db operation
+	item, err := models.GetCartItem(input.ItemId)
+	if err != nil {
+		log.Println(err)
+		c.AbortWithStatusJSON(500, resp.StdErrorResp)
+	}
+
+	c.AbortWithStatusJSON(200, gin.H{
+		"item": item,
+	})
 }
 func (ctl CartController) UpdateItem(c *gin.Context) {
-	// TODO
+	// input validation
+	input := ItemInput{}
+	if err := c.BindUri(&input); err != nil {
+		log.Println(err)
+		c.AbortWithStatusJSON(400, resp.StdErrorResp)
+	}
+
+	newItem := models.CartItem{}
+	if err := c.Bind(&newItem); err != nil {
+		log.Println(err)
+		c.AbortWithStatusJSON(400, resp.StdErrorResp)
+	}
+
+	// db operation
+	oldItem, err := models.GetCartItem(input.ItemId)
+	if err != nil {
+		log.Println(err)
+		c.AbortWithStatusJSON(500, resp.StdErrorResp)
+	}
+
+	oldItem.Amount = newItem.Amount
+	oldItem.Selects = newItem.Selects
+	oldItem.Customs = newItem.Customs
+	if tx := db.Conn().Save(&oldItem); tx.Error != nil {
+		log.Println(tx.Error)
+		c.AbortWithStatusJSON(500, resp.StdErrorResp)
+	}
+
+	c.AbortWithStatus(204)
 }
 func (ctl CartController) RemoveItem(c *gin.Context) {
-	// TODO
+	// input validation
+	input := ItemInput{}
+	if err := c.BindUri(&input); err != nil {
+		log.Println(err)
+		c.AbortWithStatusJSON(400, resp.StdErrorResp)
+	}
+
+	// db operation
+	if tx := db.Conn().Delete(&models.CartItem{}, input.ItemId); tx.Error != nil {
+		log.Println(tx.Error)
+		c.AbortWithStatusJSON(500, resp.StdErrorResp)
+	}
+
+	c.AbortWithStatus(204)
 }
--- backend/internal/httpapi/order_controller.go
+++ backend/internal/httpapi/order_controller.go
@@ -1,16 +1,57 @@
 package httpapi
 
 import (
+	"log"
 	"the-video-project/backend/internal/db"
+	"the-video-project/backend/internal/httpapi/resp"
 	"the-video-project/backend/internal/models"
+	"the-video-project/backend/internal/snowflake"
 
 	"github.com/gin-gonic/gin"
 )
 
-type OrderController struct{}
+type (
+	OrderController  struct{}
+	CreateOrderInput struct {
+		StoreId uint `form:"store_id" binding:"required" valid:"int"`
+	}
+)
 
 func (ctl OrderController) CreateOrder(c *gin.Context) {
-	// TODO
+	// input validation
+	input := CreateOrderInput{}
+	if err := c.Bind(&input); err != nil {
+		log.Println(err)
+		c.AbortWithStatusJSON(400, resp.StdErrorResp)
+	}
+
+	// get cart
+	cart := models.Cart{
+		UserId:  c.GetUint("uid"),
+		StoreId: input.StoreId,
+	}
+	if tx := db.Conn().First(&cart); tx.Error != nil {
+		log.Println(tx.Error)
+		c.AbortWithStatusJSON(404, resp.StdErrorResp)
+	}
+
+	// save order
+	order := models.Order{
+		StoreId: cart.StoreId,
+		UserId:  cart.UserId,
+		ID:      snowflake.Generate(),
+		Price:   cart.Items.CalculateTotalPrice(),
+		Items:   cart.Items,
+		Status:  0,
+	}
+	if tx := db.Conn().Save(&order); tx.Error != nil {
+		log.Println(tx.Error)
+		c.AbortWithStatusJSON(500, resp.StdErrorResp)
+	}
+
+	c.AbortWithStatusJSON(200, gin.H{
+		"order": order,
+	})
 }
 
 func (ctl OrderController) GetOrderStatus(c *gin.Context) {
