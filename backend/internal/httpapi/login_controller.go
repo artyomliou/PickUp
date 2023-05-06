24967d736eb69e4baff88ae2d131ee7235cebc53
Artyom Liou <artyomliou@gmail.com>
2023-05-07 00:24:31 +0800

    test: rewrite login_ctl_test for readability

--- backend/internal/httpapi/login_controller.go
+++ backend/internal/httpapi/login_controller.go
@@ -1,6 +1,7 @@
 package httpapi
 
 import (
+	"log"
 	"net/http"
 	"the-video-project/backend/internal/cookie"
 	"the-video-project/backend/internal/models"
@@ -9,46 +10,65 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type LoginController struct{}
+type (
+	LoginController struct{}
+	LoginBody       struct {
+		Email    string `form:"email" binding:"required"`
+		Password string `form:"password" binding:"required"`
+	}
+)
 
 func (ctl LoginController) IsLoggedIn(c *gin.Context) {
-	cookie, err := c.Cookie(cookie.CookieName)
+	tokenStr, err := c.Cookie(cookie.CookieName)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusOK, gin.H{
+			"isLoggedIn": false,
+		})
+		return
+	}
+
+	_, err = cookie.ParseTokenForUid(tokenStr)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusOK, gin.H{
+			"isLoggedIn": false,
+		})
+		return
+	}
+
 	c.AbortWithStatusJSON(http.StatusOK, gin.H{
-		"status":       "success",
-		"is-logged-in": len(cookie) > 0 && err == nil,
+		"isLoggedIn": true,
 	})
 }
 
 func (ctl LoginController) Login(c *gin.Context) {
-	type ReqBody struct {
-		Email    string `json:"email" binding:"required"`
-		Password string `json:"password" binding:"required"`
-	}
-	var body ReqBody
+	body := LoginBody{}
 	user := models.User{}
-	if err := c.ShouldBindJSON(&body); err != nil {
+	if err := c.Bind(&body); err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"status":  "error",
 			"message": "資料格式不符",
 		})
 	}
 	if user.GetByEmail(body.Email); user.ID == 0 {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"status":  "error",
 			"message": "帳號或密碼錯誤",
 		})
 	}
 	if !user.IsPasswordMatched(body.Password) {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"status":  "error",
 			"message": "帳號或密碼錯誤",
 		})
-	} else {
-		expiredAt := time.Now().AddDate(0, 0, 2)
-		token := cookie.CreateToken(user.ID, expiredAt)
-		c.SetCookie(cookie.CookieName, token, int(expiredAt.Unix()-time.Now().Unix()), "/", "", false, true) // todo
-		c.AbortWithStatusJSON(http.StatusNoContent, nil)
 	}
+
+	expiredAt := time.Now().AddDate(0, 0, 2)
+	token, err := cookie.CreateToken(user.ID, expiredAt)
+	if err != nil {
+		log.Println(err)
+		c.AbortWithStatusJSON(500, gin.H{
+			"message": "伺服器錯誤",
+		})
+	}
+	c.SetCookie(cookie.CookieName, token, int(expiredAt.Unix()-time.Now().Unix()), "/", "", false, true) // todo
+	c.AbortWithStatusJSON(http.StatusNoContent, nil)
 
 }
 
--- backend/internal/httpapi/login_controller_test.go
+++ backend/internal/httpapi/login_controller_test.go
@@ -9,57 +9,39 @@ import (
 	_ "the-video-project/backend/configs"
 	"the-video-project/backend/internal/command"
 	"the-video-project/backend/internal/cookie"
+	"the-video-project/backend/internal/db"
 	"the-video-project/backend/internal/models"
+	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/goccy/go-json"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
 
-func init() {
-	commands := []command.Command{
-		command.MigrateCommand{},
-		command.SeedCommand{},
-	}
-	for _, cmd := range commands {
-		if err := cmd.Run(); err != nil {
-			panic(err)
-		}
-	}
+func sendReq(router *gin.Engine, method string, uri string) *httptest.ResponseRecorder {
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(method, uri, nil)
+	router.ServeHTTP(w, req)
+	return w
 }
 
-func TestApiIsLoggedIn(t *testing.T) {
-	router := SetupRouter()
-
+func sendReqWithHeaders(router *gin.Engine, method string, uri string, headers map[string]string) *httptest.ResponseRecorder {
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/api/is-logged-in", nil)
+	req, _ := http.NewRequest(method, uri, nil)
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
 	router.ServeHTTP(w, req)
-
-	assert.Equal(t, w.Code, 200)
-	assert.JSONEq(t, w.Body.String(), `{"is-logged-in": false, "status": "success"}`)
+	return w
 }
 
-func TestApiLogin(t *testing.T) {
-	router := SetupRouter()
-
-	email := fmt.Sprintf("%s@not-exist.com", uuid.New())
-	password := "ultra_secret"
-	(&models.User{}).Create(email, password)
-
+func sendReqWithBody(router *gin.Engine, method string, uri string, body *bytes.Buffer) *httptest.ResponseRecorder {
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("POST", "/api/login", jsonBody(map[string]any{
-		"email":    email,
-		"password": password,
-	}))
+	req, _ := http.NewRequest(method, uri, body)
+	req.Header.Set("Content-type", "application/json")
 	router.ServeHTTP(w, req)
-
-	assert.Equal(t, w.Code, 204)
-	for _, k := range w.Result().Cookies() {
-		if k.Name == cookie.CookieName {
-			assert.NotEmpty(t, k.MaxAge)
-			assert.NotEmpty(t, k.Value)
-		}
-	}
+	return w
 }
 
 func jsonBody(jsonMap map[string]any) *bytes.Buffer {
@@ -67,18 +49,80 @@ func jsonBody(jsonMap map[string]any) *bytes.Buffer {
 	return bytes.NewBuffer(jsonByte)
 }
 
-func TestApiLogout(t *testing.T) {
-	router := SetupRouter()
-
-	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/api/logout", nil)
-	router.ServeHTTP(w, req)
-
-	assert.Equal(t, w.Code, 204)
-	for _, k := range w.Result().Cookies() {
-		if k.Name == cookie.CookieName {
-			assert.Equal(t, k.MaxAge, -1)
-			assert.Equal(t, k.Value, "")
-		}
+func TestLoginController(t *testing.T) {
+	// Setup
+	t.Setenv("DB_DRIVER", "sqlite")
+	if err := (&command.MigrateCommand{}).Run(); err != nil {
+		panic(err)
 	}
+	r := SetupRouter()
+
+	// Setup a user
+	email := fmt.Sprintf("%s@not-exist.com", uuid.New())
+	password := "ultra_secret"
+	passwordHash, _ := models.HashPassword(password)
+	user := models.User{
+		Email:    email,
+		Password: passwordHash,
+	}
+	db.Conn().Save(&user)
+
+	// Tests
+	t.Run("/api/is-logged-in", func(t *testing.T) {
+		t.Run("false", func(t *testing.T) {
+			w := sendReq(r, "GET", "/api/is-logged-in")
+			assert.Equal(t, 200, w.Code)
+			assert.JSONEq(t, `{"isLoggedIn": false}`, w.Body.String())
+		})
+		t.Run("true", func(t *testing.T) {
+			token, _ := cookie.CreateToken(user.ID, time.Now().AddDate(0, 0, 2))
+			w := sendReqWithHeaders(r, "GET", "/api/is-logged-in", map[string]string{
+				"Cookie": fmt.Sprintf("auth=%s", token),
+			})
+			assert.Equal(t, 200, w.Code)
+			assert.JSONEq(t, `{"isLoggedIn": true}`, w.Body.String())
+		})
+	})
+
+	t.Run("/api/login", func(t *testing.T) {
+		t.Run("Correct user/pwd", func(t *testing.T) {
+			w := sendReqWithBody(r, "POST", "/api/login", jsonBody(map[string]any{
+				"email":    email,
+				"password": password,
+			}))
+			assert.Equal(t, 204, w.Code)
+			for _, k := range w.Result().Cookies() {
+				if k.Name == cookie.CookieName {
+					assert.NotEmpty(t, k.MaxAge)
+					assert.NotEmpty(t, k.Value)
+				}
+			}
+		})
+
+		t.Run("Incorrect user/pwd", func(t *testing.T) {
+			w := sendReqWithBody(r, "POST", "/api/login", jsonBody(map[string]any{
+				"email":    "whatever@localhost",
+				"password": "12345678",
+			}))
+			assert.Equal(t, 400, w.Code)
+			for _, k := range w.Result().Cookies() {
+				if k.Name == cookie.CookieName {
+					assert.Empty(t, k.MaxAge)
+					assert.Empty(t, k.Value)
+				}
+			}
+		})
+	})
+
+	t.Run("/api/logout", func(t *testing.T) {
+		w := sendReq(r, "GET", "/api/logout")
+
+		assert.Equal(t, 204, w.Code)
+		for _, k := range w.Result().Cookies() {
+			if k.Name == cookie.CookieName {
+				assert.Equal(t, k.MaxAge, -1)
+				assert.Equal(t, k.Value, "")
+			}
+		}
+	})
 }
