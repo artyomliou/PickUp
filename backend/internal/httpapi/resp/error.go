45c41ebb39932e1ec9ea4eb3b11371964f2edc34
Artyom Liou <artyomliou@gmail.com>
2023-05-08 00:37:56 +0800

    chore: more error template for debugging

--- backend/internal/httpapi/resp/error.go
+++ backend/internal/httpapi/resp/error.go
@@ -7,3 +7,19 @@ import (
 var StdErrorResp = gin.H{
 	"message": "發生錯誤",
 }
+
+var UriErrorResp = gin.H{
+	"message": "URL參數有錯誤",
+}
+
+var BodyErrorResp = gin.H{
+	"message": "HTTP Body有錯誤",
+}
+
+var DbReadErrorResp = gin.H{
+	"message": "發生錯誤 (1001)",
+}
+
+var DbWriteErrorResp = gin.H{
+	"message": "發生錯誤 (1002)",
+}
