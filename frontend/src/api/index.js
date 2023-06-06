9e5ff8c50dd0a9aec4b1411d675716662e1946eb
muehung <hung.co24@gmail.com>
2023-06-07 00:35:58 +0800

    fix: api function file

---
+++ frontend/src/api/index.js
@@ -0,0 +1,47 @@
+const baseUrl = "http://localhost:5000";
+
+function withBaseUrl(url){
+    return `${baseUrl}/api/${url}`
+};
+
+export async function isLoginStatus(){
+    const url = withBaseUrl('is-logged-in')
+    // console.log(url)
+    const response = await fetch(url, {
+        credentials: 'include',
+    });
+    const apiStatus = await response.json();
+    // console.log(`apiStatus: ${!!apiStatus['is-logged-in']}`)
+    return !!apiStatus.isLoggedIn
+}
+
+
+export async function loginAction(email, password){
+    const res = await fetch(withBaseUrl('login'), {
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify({
+            email: email,
+            password: password
+        }),
+        headers: {
+            "content-type": "application/json",
+        }
+    });
+    console.log(res)
+    return res
+}
+
+export async function logoutAction(){
+    const res = await fetch(withBaseUrl('logout'), {
+        method: 'GET',
+        credentials: 'include',
+    });
+    return res.ok
+}
+
+// Reactivity test
+export function test(){
+    const user = '';
+    console.count(`test ${user}`)
+}
