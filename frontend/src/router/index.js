d0196acf46dc68736c8b6d16ec75c43b434ccb5e
muehung <hung.co24@gmail.com>
2023-06-12 09:51:24 +0800

    feat: new store page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,6 +5,7 @@ import PasswordView from '../views/PasswordView.vue';
 import index from '../views/indexView.vue';
 import notFound from '../views/notFound.vue';
 import registerView from '../views/registerView.vue';
+import storeView from '../views/storeView.vue';
 
 
 // export default defineComponent({
@@ -15,15 +16,17 @@ import registerView from '../views/registerView.vue';
 /**
  * 只允許「沒登入的 user」進入這頁面
  */
-async function isGuest() {
-    const store = useLoginStatusStore()
-    if (store.isLoginStatus) {
-        return {
-            path: '/'
-        }
-    }
-    return true
-}
+// async function isGuest() {
+//     const store = useLoginStatusStore()
+//     if (store.isLogin) {
+//         return {
+//             path: '/'
+//         }
+//     }
+//     return true
+// }
+
+// const store = useLoginStatusStore()
 
 
 const router = createRouter({
@@ -39,18 +42,35 @@ const router = createRouter({
             path: '/login',
             name: 'login',
             component: LoginView,
-            beforeEnter: [isGuest]
+            beforeEnter: async(to,from) => {
+                const store = useLoginStatusStore();
+                const fromPath = from.path == '/login'
+                if(store.isLogin == true || store.isLogin && fromPath) // 登入+從登入頁+去登入頁
+                to.path.replace == '/'
+            }
         },
         {
             path: '/register',
             name: 'register',
             component: registerView,
-            beforeEnter: [isGuest]
+            // redirect: () => { 
+            //     // const store = useLoginStatusStore()
+            //     if (store.isLogin) {
+            //         return {
+            //             path: '/'
+            //         }
+            //     } 
+            // }
         },
         {
             path: '/password',
             name: 'password',
             component: PasswordView
+        },
+        {
+            path: '/store',
+            name: 'store',
+            component: storeView
         },
         { 
             // form Kuro https://book.vue.tw/CH4/4-2-route-settings.html
@@ -63,11 +83,13 @@ const router = createRouter({
 
 
 // 每一個 router 都會經過
-// router.beforeEach((to) => {
-//     const store = useLoginCheckStore()
-//     if(to.meta.requiresAuth && !store.isLogined)
-//     return '/login'
-// })
+router.beforeEach( async(to, from) => {
+    const store = useLoginStatusStore();
+    const fromPath = from.path !== '/login' || '/register'
+    const toPath = to.path == '/login' || '/register'
+    if(store.isLogin && fromPath || toPath) // 登入+從登入頁+去登入頁
+    to.path.replace == '/'
+})
 
 // router.beforeEach( async(to) => {
 //     //如果已登入(isLogined)就轉到首頁
---
+++ frontend/src/views/storeView.vue
@@ -0,0 +1,127 @@
+<template>
+    <NavbarComponent />
+    <section class="sec-h sec-store">
+        <div class="banner" style="background-image: url('src/assets/img/store-banner.jpg')">
+        </div>
+        <div class="container py-3">
+            <div class="row">
+                <aside class="col-lg-4">
+                    <div class="store-info">
+                        <h2 class="store-title">神的茶手 台北車站店</h2>
+                        <address>100台北市中正區鄭州路8號</address>
+                        <p>This is a wider card with supporting text below as a natural lealittle bit longer.</p>
+                        <div class="store-tags">
+                            <span class="tag">營業中</span>
+                        </div>
+                    </div>
+                    <div class="store-classes">
+                        <ul class="nav class-list">
+                            <li class="nav-item active">特製調飲系列</li>
+                            <li class="nav-item">黑芝麻系列飲品</li>
+                            <li class="nav-item">黑芝麻系列飲品</li>
+                            <li class="nav-item">黑芝麻系列飲品</li>
+                            <li class="nav-item">黑芝麻系列飲品</li>
+                            <li class="nav-item">系列飲品</li>
+                        </ul>
+                    </div>
+                </aside>
+
+                <div class="col-lg-8 store-cards">
+                    <h4 class="series-title">特製調飲系列</h4>
+                    <div class="card-group justify-content-center cards">
+
+                        <!-- card -->
+                        <div class="store-card">
+                            <a href="/item" class="store-link">
+                                <figure class="card-img">
+                                    <img src="../assets/img/store-item2.jpg" alt="" class="img-fluid">
+                                </figure>
+                                <div class="card-info">
+                                    <h4 class="item-title">紫色特調飲</h4>
+                                    <p class="store-prize">$ 70.00</p>
+                                </div>
+                                <button class="btn store-btn">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-plus-circle"
+                                        viewBox="0 0 16 16">
+                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                        <path
+                                            d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                                    </svg>
+                                </button>
+                            </a>
+                        </div>
+
+                        <!-- card -->
+                        <div class="store-card">
+                            <a href="/item" class="store-link">
+                                <figure class="card-img">
+                                    <img src="../assets/img/store-item2.jpg" alt="" class="img-fluid">
+                                </figure>
+                                <div class="card-info">
+                                    <h4 class="item-title">紫色特調飲</h4>
+                                    <p class="store-prize">$ 70.00</p>
+                                </div>
+                                <button class="btn store-btn">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-plus-circle"
+                                        viewBox="0 0 16 16">
+                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                        <path
+                                            d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                                    </svg>
+                                </button>
+                            </a>
+                        </div>
+
+                        <!-- card -->
+                        <div class="store-card">
+                            <a href="/item" class="store-link">
+                                <figure class="card-img">
+                                    <img src="../assets/img/store-item2.jpg" alt="" class="img-fluid">
+                                </figure>
+                                <div class="card-info">
+                                    <h4 class="item-title">紫色特調飲</h4>
+                                    <p class="store-prize">$ 70.00</p>
+                                </div>
+                                <button class="btn store-btn">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-plus-circle"
+                                        viewBox="0 0 16 16">
+                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                        <path
+                                            d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                                    </svg>
+                                </button>
+                            </a>
+                        </div>
+
+                        <!-- card -->
+                        <div class="store-card">
+                            <a href="/item" class="store-link">
+                                <figure class="card-img">
+                                    <img src="../assets/img/store-item2.jpg" alt="" class="img-fluid">
+                                </figure>
+                                <div class="card-info">
+                                    <h4 class="item-title">紫色特調飲</h4>
+                                    <p class="store-prize">$ 70.00</p>
+                                </div>
+                                <button class="btn store-btn">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-plus-circle"
+                                        viewBox="0 0 16 16">
+                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                        <path
+                                            d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                                    </svg>
+                                </button>
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <FooterComponent />
+</template>
+
+<script>
+import "../assets/css/store.css";
+</script>
