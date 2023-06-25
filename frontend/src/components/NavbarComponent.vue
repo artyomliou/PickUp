9a91e11555e0dddcc4b30c320634a5da9389aea5
muehung <hung.co24@gmail.com>
2023-06-25 22:56:41 +0800

    feat: navibar checkout link

--- frontend/src/components/NavbarComponent.vue
+++ frontend/src/components/NavbarComponent.vue
@@ -30,21 +30,24 @@
                     <span class="location-txt">目前地址</span>
                 </button>
                 <template v-if="statusStore == false">
-                <div class="col-auto ms-auto">
-                    <p class="d-inline-block">您尚未登入</p>
-                    <a href="/login" class="btn btn-login">登入</a>
-                    <a href="/register" class="btn btn-register">註冊</a>
-                </div>
+                    <div class="col-auto ms-auto">
+                        <p class="d-inline-block">您尚未登入</p>
+                        <a href="/login" class="btn btn-login">登入</a>
+                        <a href="/register" class="btn btn-register">註冊</a>
+                    </div>
                 </template>
                 <template v-else>
-                    <button type="button" class="btn btn-cart ms-auto">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
-                            <path
-                                d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
-                        </svg>
-                        <span class="cart-txt">購物車</span> •
-                        <span class="cart-num">0</span>
-                    </button>
+                    <router-link to="/Checkout" custom v-slot="{ navigate }">
+                        <button @click="navigate" class="btn btn-cart ms-auto" rol="link">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-cart4"
+                                viewBox="0 0 16 16">
+                                <path
+                                    d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+                            </svg>
+                            <span class="cart-txt">購物車</span> •
+                            <span class="cart-num">0</span>
+                        </button>
+                    </router-link>
                 </template>
 
             </div>
