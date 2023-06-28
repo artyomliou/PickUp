fbb2c677c4c43619a6dee243ed75d17805ae58da
muehung <hung.co24@gmail.com>
2023-06-28 10:15:42 +0800

    feat(cart): navbar add checkout link

--- frontend/src/components/NavbarComponent.vue
+++ frontend/src/components/NavbarComponent.vue
@@ -2,7 +2,6 @@
     <div>
         <div class="shadow" :class="{ show: sidebarShow }" @click="sidebarToggle"></div>
     </div>
-
     <nav class="navbar navbar-expand-lg navbar-light main-nav" id="navMain">
         <div class="container-fluid">
             <button @click="sidebarToggle" class="navbar-hamburger" type="button" role="button" data-bs-toggle="collapse"
@@ -37,16 +36,16 @@
                     </div>
                 </template>
                 <template v-else>
-                    <router-link to="/Checkout" custom v-slot="{ navigate }">
-                        <button @click="navigate" class="btn btn-cart ms-auto" rol="link">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-cart4"
-                                viewBox="0 0 16 16">
-                                <path
-                                    d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
-                            </svg>
-                            <span class="cart-txt">購物車</span> •
-                            <span class="cart-num">0</span>
-                        </button>
+                    <router-link v-show="$route.params.storeId" :to="`/store/${$route.params.storeId}/checkout`"
+                        class="btn btn-cart ms-auto" rol="link">
+                        <!-- <button @click="navigate" class="btn btn-cart ms-auto" rol="link"> -->
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+                            <path
+                                d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+                        </svg>
+                        <span class="cart-txt">購物車</span> •
+                        <span class="cart-num">0</span>
+                        <!-- </button> -->
                     </router-link>
                 </template>
 
@@ -92,14 +91,17 @@ import { mapWritableState } from 'pinia';
 import { logoutAction } from '../api/index';
 import router from '../router';
 import { useLoginStatusStore } from '../stores/useLoginStatusStore';
+// import { useStoreIdStore } from '../stores/useStoreIdStore';
 export default {
     data() {
         return {
             sidebarShow: false, //側邊欄隱藏
+            store: {},
         }
     },
     computed: {
         ...mapWritableState(useLoginStatusStore, ['isLogin']),
+        // ...mapState(useStoreIdStore, ['storeId']),
         statusStore() {
             const store = useLoginStatusStore();
             // console.log('is value--')
@@ -123,7 +125,10 @@ export default {
             } else {
                 console.log('It is logout')
             }
-        },
+        }
+    },
+    async mounted() {
+
     }
 }
 </script>
