8d24d2360b53d0401db46a3e5e4aa28f0010abac
muehung <hung.co24@gmail.com>
2023-11-10 23:47:09 +0800

    feat: productView select questions

--- frontend/src/views/productView.vue
+++ frontend/src/views/productView.vue
@@ -3,7 +3,7 @@
         <div class="container py-3">
             <div class="row">
                 <div class="col-12 py-3">
-                    <!-- {{ productInfo }} -->
+                    <!-- {{ product }} -->
                 </div>
                 <div class="col-lg-6 product-picarea">
                     <div class="nav-sticky">
@@ -18,10 +18,13 @@
                         <figure>
                             <img src="../assets/img/product/gold-olong.jpg" alt="" class="img-fluid">
                         </figure>
-
+                        <div v-for="item in product.selectQuestions" :key="item.id">
+                            <!-- <div class="card my-3">{{ item }}</div> -->
+                            <div class="card my-3">{{ item.options }}</div>
+                        </div>
                     </div>
                 </div>
-                <div class="col-lg-6 product-infoarea mt-5">
+                <div class=" col-lg-6 product-infoarea mt-5">
                     <h2 class="product-title">{{ product.name }}</h2>
                     <h5 class="product-price">$ {{ product.price }}</h5>
                     <p class="product-intro">{{ product.intro }}</p>
@@ -36,24 +39,22 @@
                                 <span class="tag tag-reqired ms-auto">必填</span>
 
                                 <!-- 必填沒填時 顯現 -->
-                                <span class="tag tag-reqired ms-auto bg-danger text-white">
+                                <!-- <span class="tag tag-reqired ms-auto bg-danger text-white">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                         class="bi bi-check" viewBox="0 0 16 16">
                                         <path
                                             d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
                                     </svg> 必填
-                                </span>
+                                </span> -->
                                 <!-- 必填沒填時 顯現 -->
-
-                                <template v-for="select in   quiz.options" :key="select.id">
-                                    <!-- select: {{ select }}<br> -->
-
-                                    <label :for="`quiz${select.selectQuestionId}`"
-                                        class="form-control my-2 d-flex justify-content-between">
-                                        <span>{{ select.name }}</span>
-                                        <input v-model="productInfo.selectQuestionId" true-value="selectQuestionId"
-                                            false-value="" type="radio" :name="`quiz${select.selectQuestionId}`"
-                                            class="radio">
+                                {{ customAnswer }}
+                                <template v-for="option in   quiz.options" :key="option.id">
+                                    <label class="form-control my-2 d-flex justify-content-between">
+                                        <span>{{ option.name }}</span>
+                                        <!-- selectQuestionId -->
+                                        <input type="radio" :name="quiz.id" :value="option.id"
+                                            v-model="customAnswer[quiz.id]" class="radio"> <!-- like map -->
+                                        {{ option.selectQuestionId }}
                                     </label>
                                 </template>
 
@@ -66,7 +67,7 @@
                                         <p class="select-name strong selectName">{{ frSelect.name }}</p>
                                         <p class="select-info">{{ frSelect.info }}</p>
                                         <p class="price">$ {{ frSelect.price }}</p>
-                                        <input v-model="productInfo.selectQuestionId" :value="`otherAdded${frSelect.id}`"
+                                        <input v-model="product.selectQuestionId" :value="`otherAdded${frSelect.id}`"
                                             type="checkbox" class="radio">
                                     </label>
                                 </template>
@@ -76,14 +77,14 @@
                         <template v-for="quizCustom in  product.customQuestions " :key="quizCustom.id">
                             <label for="" class="">
                                 <h5 class="head">{{ quizCustom.name }}</h5>
-                                <textarea v-model="productInfo.notes" class="form-control form-textarea" name=""
+                                <textarea v-model="product.notes" class="form-control form-textarea" name=""
                                     :id="quizCustom.id" cols="30" rows="5" :placeholder="quizCustom.placeholder"></textarea>
                             </label>
                         </template>
 
                         <label for="" class="py-3 d-flex">
                             <span class="selectName">數量</span>
-                            <select v-model="productInfo.cups" id="" class="ms-3">
+                            <select v-model="product.cups" id="" class="ms-3">
                                 <option value="1" selected>1</option>
                                 <option value="2">2</option>
                                 <option value="3">3</option>
@@ -93,7 +94,7 @@
                         <div class="d-grid py-3">
                             <button type="submit" class="btn btn-submit p-3">
                                 新增
-                                <span class="totalCount">{{ productInfo.cups }}</span> 產品
+                                <span class="totalCount">{{ product.cups }}</span> 產品
                                 • 至購物車
                             </button>
                         </div>
@@ -115,15 +116,8 @@ export default {
         return {
             /** @type {Store} */
             product: {},
-            productInfo: {
-                selectQuestionId: [],
-                selectQuestions: '',
-                ice: '',
-                suger: '',
-                add: '',
-                notes: '',
-                cups: 1,
-            }
+            customAnswer: {},
+
         }
     },
     async created() {
@@ -131,10 +125,10 @@ export default {
         const prodId = this.$route.params.productId;
         const data = await productApi(storeId, prodId);
         this.product = data;
-        console.log(data);
+        // console.log(data);
         // this.product = data;
         // renderData(data);
-        // console.log(this.productInfo.cups)
+        // console.log(this.product.cups)
     },
     methods: {
         submitProdunctForm(e) {
